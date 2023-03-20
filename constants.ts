2e4f0aa79eccbc9f53cf782845e162663a5f6a7c
Oleksii Ivashchenko <ivash.lex@gmail.com>
2023-03-20 22:25:03 +0100

    added POM pattern. Added tests for swagLabs page

---
+++ constants.ts
@@ -0,0 +1,8 @@
+export const BASE_URL = 'https://www.saucedemo.com';
+
+export const STANDARD_USER = 'standard_user';
+export const LOCKED_OUT_USER = 'locked_out_user';
+export const PROBLEM_USER = 'problem_user';
+export const PERFORMANCE_GLITCH_USER = 'performance_glitch_user';
+
+export const PASS = 'secret_sauce';
